b311f6aba82cd93b2d748b8e47c6f8d29a21746d
Johannes Kron <kron@webrtc.org>
2023-02-03 11:01:52 +0000

    Add UMA histograms to track usage of fullscreen detection
    Bug: chromium:1348011
    Change-Id: I3219e74c49ff77e00b2224c8cf82f78d1e0fd9cf
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/291708
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Commit-Queue: Johannes Kron <kron@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#39254}

--- modules/desktop_capture/desktop_capturer.cc
+++ modules/desktop_capture/desktop_capturer.cc
@@ -19,6 +19,7 @@
 #include "modules/desktop_capture/cropping_window_capturer.h"
 #include "modules/desktop_capture/desktop_capture_options.h"
 #include "modules/desktop_capture/desktop_capturer_differ_wrapper.h"
+#include "system_wrappers/include/metrics.h"
 
 #if defined(RTC_ENABLE_WIN_WGC)
 #include "modules/desktop_capture/win/wgc_capturer_win.h"
@@ -26,6 +27,10 @@
 #endif  // defined(RTC_ENABLE_WIN_WGC)
 
 namespace webrtc {
+
+void LogDesktopCapturerFullscreenDetectorUsage() {
+  RTC_HISTOGRAM_BOOLEAN("WebRTC.Screenshare.DesktopCapturerUsage", true);
+}
 
 DesktopCapturer::~DesktopCapturer() = default;
 
