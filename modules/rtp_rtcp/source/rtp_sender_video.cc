490f0b82d7729078f572d587275b5fa875834ea1
Danil Chapovalov <danilchap@webrtc.org>
2023-08-08 13:07:32 +0200

    Cleanup usage of csrcs in RtpSenderVideoFrameTransform
    CSRCs are decided on a per frame bases, thus keeping a constant copy of
    csrcs inside the rtp sender transform delegate is confusing: when transform delegate is created, csrcs list is always empty.
    
    Bug: None
    Change-Id: Id94acc76857a47ad9a1dd8254648ab9cb5d6d31d
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/311840
    Reviewed-by: Tony Herre <herre@google.com>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#40533}

--- modules/rtp_rtcp/source/rtp_sender_video.cc
+++ modules/rtp_rtcp/source/rtp_sender_video.cc
@@ -158,7 +158,6 @@ RTPSenderVideo::RTPSenderVideo(const Config& config)
                     this,
                     config.frame_transformer,
                     rtp_sender_->SSRC(),
-                    rtp_sender_->Csrcs(),
                     config.task_queue_factory)
               : nullptr) {
   if (frame_transformer_delegate_)
--- modules/rtp_rtcp/source/rtp_sender_video_frame_transformer_delegate.cc
+++ modules/rtp_rtcp/source/rtp_sender_video_frame_transformer_delegate.cc
@@ -119,12 +119,10 @@ RTPSenderVideoFrameTransformerDelegate::RTPSenderVideoFrameTransformerDelegate(
     RTPVideoFrameSenderInterface* sender,
     rtc::scoped_refptr<FrameTransformerInterface> frame_transformer,
     uint32_t ssrc,
-    std::vector<uint32_t> csrcs,
     TaskQueueFactory* task_queue_factory)
     : sender_(sender),
       frame_transformer_(std::move(frame_transformer)),
       ssrc_(ssrc),
-      csrcs_(csrcs),
       transformation_queue_(task_queue_factory->CreateTaskQueue(
           "video_frame_transformer",
           TaskQueueFactory::Priority::NORMAL)) {}
@@ -143,7 +141,8 @@ bool RTPSenderVideoFrameTransformerDelegate::TransformFrame(
     TimeDelta expected_retransmission_time) {
   frame_transformer_->Transform(std::make_unique<TransformableVideoSenderFrame>(
       encoded_image, video_header, payload_type, codec_type, rtp_timestamp,
-      expected_retransmission_time, ssrc_, csrcs_));
+      expected_retransmission_time, ssrc_,
+      /*csrcs=*/std::vector<uint32_t>()));
   return true;
 }
 
