4ef01d41b73c1543abf1096e64406ae5233d0230
Mirko Bonadei <mbonadei@webrtc.org>
2023-08-29 18:33:22 +0000

    Revert "Per default enable RobustThroughputEstimator"
    This reverts commit d017b1e306186252ed52ab84459d05efc4eb9fd4.
    
    Reason for revert: Breaks downstream test.
    
    Original change's description:
    > Per default enable RobustThroughputEstimator
    >
    > Experiments has not showed significant metric changes. However, simulations has showed that RobustThroughputEstimator better follow the actually receive rate better. Especially during bursts of sent packets. Code is also simpler.
    >
    >
    > Bug: webrtc:13402 chromium:1411666
    > Change-Id: I38c309f74e8e1322602196354545b3a465866263
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/318040
    > Reviewed-by: Björn Terelius <terelius@webrtc.org>
    > Commit-Queue: Per Kjellander <perkj@webrtc.org>
    > Cr-Commit-Position: refs/heads/main@{#40653}
    
    Bug: webrtc:13402 chromium:1411666 b/298001595
    Change-Id: Ic68ef954f462021e991f3183b94d85eb6a44fac0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/318141
    Owners-Override: Mirko Bonadei <mbonadei@webrtc.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#40658}

--- modules/congestion_controller/goog_cc/acknowledged_bitrate_estimator_interface.h
+++ modules/congestion_controller/goog_cc/acknowledged_bitrate_estimator_interface.h
@@ -33,9 +33,7 @@ struct RobustThroughputEstimatorSettings {
   explicit RobustThroughputEstimatorSettings(
       const FieldTrialsView* key_value_config);
 
-  // Set `enabled` to true to use the RobustThroughputEstimator, false to use
-  // the AcknowledgedBitrateEstimator.
-  bool enabled = true;
+  bool enabled = false;  // Set to true to use RobustThroughputEstimator.
 
   // The estimator keeps the smallest window containing at least
   // `window_packets` and at least the packets received during the last
