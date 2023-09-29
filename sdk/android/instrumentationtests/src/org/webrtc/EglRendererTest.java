dd15070b4539d49ae5e135e7d12b64bbc7bafb27
Linus Nilsson <lnilsson@webrtc.org>
2023-09-29 13:10:04 +0200

    Adopt EglThread in EglRenderer once again.
    The regression obseverved on Samung devices the last time was caused
    by the not detaching the context/surface prior to releasing an
    EGLSurface or EGLContext. This was fine on most devices but obviously
    not all.
    
    Bug: b/225229697
    Change-Id: I1849c772f3ed3e8819c748d997e5261289c4b2bc
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/321842
    Commit-Queue: Linus Nilsson <lnilsson@webrtc.org>
    Reviewed-by: Zoé Lepaul <xalep@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#40844}

--- sdk/android/instrumentationtests/src/org/webrtc/EglRendererTest.java
+++ sdk/android/instrumentationtests/src/org/webrtc/EglRendererTest.java
@@ -103,6 +103,7 @@ public class EglRendererTest {
   final TestFrameListener testFrameListener = new TestFrameListener();
 
   EglRenderer eglRenderer;
+  EglThread eglThread;
   CountDownLatch surfaceReadyLatch = new CountDownLatch(1);
   int oesTextureId;
   SurfaceTexture surfaceTexture;
@@ -113,8 +114,10 @@ public class EglRendererTest {
                                          .builder(InstrumentationRegistry.getTargetContext())
                                          .setNativeLibraryName(TestConstants.NATIVE_LIBRARY)
                                          .createInitializationOptions());
+    eglThread =
+        EglThread.create(null /* releaseMonitor */, null /* sharedContext */, EglBase.CONFIG_RGBA);
     eglRenderer = new EglRenderer("TestRenderer: ");
-    eglRenderer.init(null /* sharedContext */, EglBase.CONFIG_RGBA, new GlRectDrawer());
+    eglRenderer.init(eglThread, new GlRectDrawer(), false /* usePresentationTimeStamp */);
     oesTextureId = GlUtil.generateTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES);
     surfaceTexture = new SurfaceTexture(oesTextureId);
     surfaceTexture.setDefaultBufferSize(1 /* width */, 1 /* height */);
@@ -244,6 +247,12 @@ public class EglRendererTest {
     }
   }
 
+  private void drainRenderThread() throws Exception {
+    CountDownLatch latch = new CountDownLatch(1);
+    eglThread.getHandler().post(() -> latch.countDown());
+    latch.await();
+  }
+
   /** Tells eglRenderer to render test frame with given index. */
   private void feedFrame(int i) {
     final VideoFrame.I420Buffer buffer = JavaI420Buffer.wrap(TEST_FRAME_WIDTH, TEST_FRAME_HEIGHT,
@@ -348,6 +357,22 @@ public class EglRendererTest {
         testFrameListener, 1f /* scaleFactor */, null, true /* applyFpsReduction */);
     feedFrame(1);
     assertFalse(testFrameListener.waitForBitmap(RENDER_WAIT_MS));
+  }
+
+  @Test
+  @SmallTest
+  public void testRecreateSurface() throws Exception {
+    // Make sure the EGLSurface has been created.
+    drainRenderThread();
+
+    // Relase the surface and wait for completion.
+    CountDownLatch latch = new CountDownLatch(1);
+    eglRenderer.releaseEglSurface(() -> latch.countDown());
+    latch.await();
+
+    // Recreate the surface.
+    eglRenderer.createEglSurface(surfaceTexture);
+    drainRenderThread();
   }
 
   private static ByteBuffer[][] copyTestDataToDirectByteBuffers(byte[][][] testData) {
