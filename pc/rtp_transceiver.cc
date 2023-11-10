33c7edd58ad0edc71939b9372fff3ab563c1f4a7
Philipp Hancke <phancke@microsoft.com>
2023-11-10 13:51:31 +0100

    Enable DD and VLA header extensions by default for Simulcast/SVC
    When Simulcast (more than one encoding) or SVC (a scalability mode
    other than the default L1T1) is used, enable the AV1 Dependency
    Descriptor and the video-layer-allocations RTP header extensions by
    default.
    
    The RTP header extensions API can be used to disable them if needed.
    
    BUG=webrtc:15378
    
    Change-Id: I587ac32c9d681461496a136f6950b007e72da86d
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/326100
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    Commit-Queue: Philipp Hancke <phancke@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#41332}

--- pc/rtp_transceiver.cc
+++ pc/rtp_transceiver.cc
@@ -177,6 +177,23 @@ RtpTransceiver::RtpTransceiver(
           : media_engine()->voice().send_codecs());
   senders_.push_back(sender);
   receivers_.push_back(receiver);
+
+  // Set default header extensions depending on whether simulcast/SVC is used.
+  RtpParameters parameters = sender->internal()->GetParametersInternal();
+  bool uses_simulcast = parameters.encodings.size() > 1;
+  bool uses_svc = !parameters.encodings.empty() &&
+                  parameters.encodings[0].scalability_mode.has_value() &&
+                  parameters.encodings[0].scalability_mode !=
+                      ScalabilityModeToString(ScalabilityMode::kL1T1);
+  if (uses_simulcast || uses_svc) {
+    // Enable DD and VLA extensions, can be deactivated by the API.
+    for (RtpHeaderExtensionCapability& ext : header_extensions_to_negotiate_) {
+      if (ext.uri == RtpExtension::kVideoLayersAllocationUri ||
+          ext.uri == RtpExtension::kDependencyDescriptorUri) {
+        ext.direction = RtpTransceiverDirection::kSendRecv;
+      }
+    }
+  }
 }
 
 RtpTransceiver::~RtpTransceiver() {
