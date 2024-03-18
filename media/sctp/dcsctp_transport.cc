1d26fd33caa35125ed1ed8dc313c570851b2b179
Tommi <tommi@webrtc.org>
2024-03-18 17:57:52 +0100

    Replace SignalClosed sigslot with absl::AnyInvocable
    This restricts the interface such that only a single onclose handler
    can be set and that only one OnClose() notification will be fired.
    
    That behavior is the same as how the previous sigslot was being
    used, but the difference is that, in addition to removing sigslot,
    this pattern is now more explicitly checked in the design.
    
    Bug: webrtc:11943
    Change-Id: I469c8cab3d62544988c8145b326af60b06b76d8e
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/343340
    Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
    Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#41920}

--- media/sctp/dcsctp_transport.cc
+++ media/sctp/dcsctp_transport.cc
@@ -617,7 +617,13 @@ void DcSctpTransport::ConnectTransportSignals() {
                 const rtc::ReceivedPacket& packet) {
         OnTransportReadPacket(transport, packet);
       });
-  transport_->SignalClosed.connect(this, &DcSctpTransport::OnTransportClosed);
+  transport_->SetOnCloseCallback([this]() {
+    RTC_DCHECK_RUN_ON(network_thread_);
+    RTC_DLOG(LS_VERBOSE) << debug_name_ << "->OnTransportClosed().";
+    if (data_channel_sink_) {
+      data_channel_sink_->OnTransportClosed({});
+    }
+  });
 }
 
 void DcSctpTransport::DisconnectTransportSignals() {
@@ -627,7 +633,7 @@ void DcSctpTransport::DisconnectTransportSignals() {
   }
   transport_->SignalWritableState.disconnect(this);
   transport_->DeregisterReceivedPacketCallback(this);
-  transport_->SignalClosed.disconnect(this);
+  transport_->SetOnCloseCallback(nullptr);
 }
 
 void DcSctpTransport::OnTransportWritableState(
@@ -653,15 +659,6 @@ void DcSctpTransport::OnTransportReadPacket(
                        << packet.payload().size();
   if (socket_) {
     socket_->ReceivePacket(packet.payload());
-  }
-}
-
-void DcSctpTransport::OnTransportClosed(
-    rtc::PacketTransportInternal* transport) {
-  RTC_DCHECK_RUN_ON(network_thread_);
-  RTC_DLOG(LS_VERBOSE) << debug_name_ << "->OnTransportClosed().";
-  if (data_channel_sink_) {
-    data_channel_sink_->OnTransportClosed({});
   }
 }
 
