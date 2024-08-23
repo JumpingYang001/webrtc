2e376cd36dc4beb224c2a4b858841a6b46a1c5df
Jonas Oreland <jonaso@google.com>
2024-08-23 07:04:28 +0000

    Revert "Add first iteration of PayloadTypePicker.SuggestPayloadType"
    This reverts commit e2869de9efb8633e51815061242fe5eefd43dad7.
    
    Reason for revert: pr-answer
    
    Original change's description:
    > Add first iteration of PayloadTypePicker.SuggestPayloadType
    >
    > Bug: webrtc:360058654
    > Change-Id: I8f9242a97dc871a39ae72f325b8ca039b2285bae
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/360061
    > Commit-Queue: Harald Alvestrand <hta@webrtc.org>
    > Reviewed-by: Florent Castelli <orphis@webrtc.org>
    > Cr-Commit-Position: refs/heads/main@{#42828}
    
    Bug: webrtc:360058654
    Change-Id: I3a6b20595aa8420983f692048a8abdb254bf20d8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/360343
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Henrik Boström <hbos@webrtc.org>
    Commit-Queue: Jonas Oreland <jonaso@google.com>
    Cr-Commit-Position: refs/heads/main@{#42833}

--- pc/payload_type_picker.h
+++ pc/payload_type_picker.h
@@ -12,7 +12,6 @@
 #define PC_PAYLOAD_TYPE_PICKER_H_
 
 #include <map>
-#include <set>
 #include <utility>
 #include <vector>
 
@@ -30,35 +29,16 @@ class PayloadType : public StrongAlias<class PayloadTypeTag, uint8_t> {
   constexpr operator uint8_t() const& { return value_; }  // NOLINT: Explicit
 };
 
-class PayloadTypeRecorder;
-
 class PayloadTypePicker {
  public:
-  PayloadTypePicker();
+  PayloadTypePicker() = default;
   PayloadTypePicker(const PayloadTypePicker&) = delete;
   PayloadTypePicker& operator=(const PayloadTypePicker&) = delete;
   PayloadTypePicker(PayloadTypePicker&&) = delete;
   PayloadTypePicker& operator=(PayloadTypePicker&&) = delete;
-  // Suggest a payload type for the codec.
-  // If the excluder maps it to something different, don't suggest it.
-  RTCErrorOr<PayloadType> SuggestMapping(cricket::Codec codec,
-                                         PayloadTypeRecorder* excluder);
+
+  RTCErrorOr<PayloadType> SuggestMapping(cricket::Codec codec);
   RTCError AddMapping(PayloadType payload_type, cricket::Codec codec);
-
- private:
-  class MapEntry {
-   public:
-    MapEntry(PayloadType payload_type, cricket::Codec codec)
-        : payload_type_(payload_type), codec_(codec) {}
-    PayloadType payload_type() const { return payload_type_; }
-    cricket::Codec codec() const { return codec_; }
-
-   private:
-    PayloadType payload_type_;
-    cricket::Codec codec_;
-  };
-  std::vector<MapEntry> entries_;
-  std::set<PayloadType> seen_payload_types_;
 };
 
 class PayloadTypeRecorder {
