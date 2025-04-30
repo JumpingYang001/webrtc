cd107c00ae6598f55c636a77be83987607afa577
Philipp Hancke <phancke@meta.com>
2025-04-30 10:40:43 -0700

    Remove DTLS record parsing utils
    since we no longer use the record numbers for acks
    
    BUG=webrtc:367395350
    
    Change-Id: Iccee634225fe27dc23337519ab5c699b654d9672
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/389161
    Reviewed-by: Jonas Oreland <jonaso@webrtc.org>
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Commit-Queue: Philipp Hancke <phancke@meta.com>
    Cr-Commit-Position: refs/heads/main@{#44504}

--- p2p/dtls/dtls_utils.cc
+++ p2p/dtls/dtls_utils.cc
@@ -13,13 +13,11 @@
 #include <algorithm>
 #include <cstdint>
 #include <memory>
-#include <optional>
 #include <vector>
 
 #include "absl/container/flat_hash_set.h"
 #include "api/array_view.h"
 #include "rtc_base/buffer.h"
-#include "rtc_base/byte_buffer.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/crc32.h"
 
@@ -28,11 +26,6 @@ namespace {
 const uint8_t kDtlsChangeCipherSpecRecord = 20;
 const uint8_t kDtlsHandshakeRecord = 22;
 
-// https://www.rfc-editor.org/rfc/rfc9147.html#section-4
-const uint8_t kFixedBitmask = 0b00100000;
-const uint8_t kConnectionBitmask = 0b00010000;
-const uint8_t kSequenceNumberBitmask = 0b00001000;
-const uint8_t kLengthPresentBitmask = 0b00000100;
 }  // namespace
 
 namespace webrtc {
@@ -60,90 +53,6 @@ bool IsDtlsHandshakePacket(ArrayView<const uint8_t> payload) {
   // message which starts with a handshake record (22) again.
   return payload.size() > 17 && (payload[0] == kDtlsHandshakeRecord ||
                                  payload[0] == kDtlsChangeCipherSpecRecord);
-}
-
-// Returns a (unsorted) list of (msg_seq) received as part of the handshake.
-std::optional<std::vector<uint16_t>> GetDtlsHandshakeAcks(
-    ArrayView<const uint8_t> dtls_packet) {
-  std::vector<uint16_t> acks;
-  ByteBufferReader record_buf(dtls_packet);
-  // https://datatracker.ietf.org/doc/html/rfc6347#section-4.1
-  while (record_buf.Length() >= kDtlsRecordHeaderLen) {
-    uint8_t content_type;
-    uint64_t epoch_and_seq;
-    uint16_t len;
-    // Read content_type(1).
-    if (!record_buf.ReadUInt8(&content_type)) {
-      return std::nullopt;
-    }
-
-    // DTLS 1.3 rules:
-    // https://www.rfc-editor.org/rfc/rfc9147.html#section-4.1
-    if ((content_type & kFixedBitmask) == kFixedBitmask) {
-      // Interpret as DTLSCipherText:
-      // https://www.rfc-editor.org/rfc/rfc9147.html#appendix-A.1
-      // We assume no connection id is used so C must be 0.
-      if ((content_type & kConnectionBitmask) != 0) {
-        return std::nullopt;
-      }
-      // Skip sequence_number(1 or 2 bytes depending on S bit).
-      if (!record_buf.Consume((content_type & kSequenceNumberBitmask) ==
-                                      kSequenceNumberBitmask
-                                  ? 2
-                                  : 1)) {
-        return std::nullopt;
-      }
-      // If the L bit is set, consume the 16 bit length field.
-      if ((content_type & kLengthPresentBitmask) == kLengthPresentBitmask) {
-        if (!(record_buf.ReadUInt16(&len) && record_buf.Consume(len))) {
-          return std::nullopt;
-        }
-      }
-      // DTLSCipherText is encrypted so we can not read it.
-      continue;
-    }
-    // Skip version(2), read epoch+seq(2+6), read len(2)
-    if (!(record_buf.Consume(2) && record_buf.ReadUInt64(&epoch_and_seq) &&
-          record_buf.ReadUInt16(&len) && record_buf.Length() >= len)) {
-      return std::nullopt;
-    }
-    if (content_type != kDtlsHandshakeRecord) {
-      record_buf.Consume(len);
-      continue;
-    }
-    // Epoch 1+ is encrypted so we can not parse it.
-    if (epoch_and_seq >> 6 != 0) {
-      record_buf.Consume(len);
-      continue;
-    }
-
-    // https://www.rfc-editor.org/rfc/rfc6347.html#section-4.2.2
-    ByteBufferReader handshake_buf(record_buf.DataView().subview(0, len));
-    while (handshake_buf.Length() > 0) {
-      uint16_t msg_seq;
-      uint32_t fragment_len;
-      uint32_t fragment_offset;
-      // Skip msg_type(1) and length(3), read msg_seq(2), skip
-      // fragment_offset(3), read fragment_length(3) and consume it.
-      if (!(handshake_buf.Consume(1 + 3) &&
-            handshake_buf.ReadUInt16(&msg_seq) &&
-            handshake_buf.ReadUInt24(&fragment_offset) &&
-            handshake_buf.ReadUInt24(&fragment_len) &&
-            handshake_buf.Consume(fragment_len))) {
-        return std::nullopt;
-      }
-      acks.push_back(msg_seq);
-      // Advance outer buffer.
-      record_buf.Consume(12 + fragment_len);
-    }
-    RTC_DCHECK(handshake_buf.Length() == 0);
-  }
-
-  // Should have consumed everything.
-  if (record_buf.Length() != 0) {
-    return std::nullopt;
-  }
-  return acks;
 }
 
 uint32_t ComputeDtlsPacketHash(ArrayView<const uint8_t> dtls_packet) {
