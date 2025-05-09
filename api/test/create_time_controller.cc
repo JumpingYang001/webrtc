83dac84ea01ec66634804fe88ebedca9ea9859b8
Danil Chapovalov <danilchap@webrtc.org>
2025-05-09 14:56:51 +0200

    Delete deprecated EnableMediaWithDefaultsAndTimeController
    Custom clock for testing can be injected via
    PeerConnectionFactoryDependencies::env
    
    Bug: None
    Change-Id: I35a9de6d974f12524ca7335434cd94aca9ee0570
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/390680
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#44610}

--- api/test/create_time_controller.cc
+++ api/test/create_time_controller.cc
@@ -11,21 +11,9 @@
 #include "api/test/create_time_controller.h"
 
 #include <memory>
-#include <utility>
 
-#include "absl/base/nullability.h"
-#include "api/enable_media_with_defaults.h"
-#include "api/environment/environment.h"
-#include "api/environment/environment_factory.h"
-#include "api/peer_connection_interface.h"
 #include "api/test/time_controller.h"
 #include "api/units/timestamp.h"
-#include "call/call.h"
-#include "call/call_config.h"
-#include "media/base/media_engine.h"
-#include "pc/media_factory.h"
-#include "rtc_base/checks.h"
-#include "system_wrappers/include/clock.h"
 #include "test/time_controller/simulated_time_controller.h"
 
 namespace webrtc {
@@ -33,41 +21,6 @@ namespace webrtc {
 std::unique_ptr<TimeController> CreateSimulatedTimeController() {
   return std::make_unique<GlobalSimulatedTimeController>(
       Timestamp::Seconds(10000));
-}
-
-void EnableMediaWithDefaultsAndTimeController(
-    TimeController& time_controller,
-    PeerConnectionFactoryDependencies& deps) {
-  class TimeControllerBasedFactory : public MediaFactory {
-   public:
-    TimeControllerBasedFactory(
-        Clock* absl_nonnull clock,
-        absl_nonnull std::unique_ptr<MediaFactory> media_factory)
-        : clock_(clock), media_factory_(std::move(media_factory)) {}
-
-    std::unique_ptr<Call> CreateCall(CallConfig config) override {
-      EnvironmentFactory env_factory(config.env);
-      env_factory.Set(clock_);
-
-      config.env = env_factory.Create();
-      return media_factory_->CreateCall(std::move(config));
-    }
-
-    std::unique_ptr<MediaEngineInterface> CreateMediaEngine(
-        const Environment& env,
-        PeerConnectionFactoryDependencies& dependencies) override {
-      return media_factory_->CreateMediaEngine(env, dependencies);
-    }
-
-   private:
-    Clock* absl_nonnull clock_;
-    absl_nonnull std::unique_ptr<MediaFactory> media_factory_;
-  };
-
-  EnableMediaWithDefaults(deps);
-  RTC_CHECK(deps.media_factory);
-  deps.media_factory = std::make_unique<TimeControllerBasedFactory>(
-      time_controller.GetClock(), std::move(deps.media_factory));
 }
 
 }  // namespace webrtc
--- api/test/create_time_controller.h
+++ api/test/create_time_controller.h
@@ -12,21 +12,12 @@
 
 #include <memory>
 
-#include "api/peer_connection_interface.h"
 #include "api/test/time_controller.h"
 
 namespace webrtc {
 
 // Creates a time controller that runs in simulated time.
 std::unique_ptr<TimeController> CreateSimulatedTimeController();
-
-// Adjusts media `deps` to use clock `time_controller` provides, fills media
-// related dependencies, and enables media support for a PeerConnectionFactory
-// created from `deps`.
-[[deprecated("Inject Clock through the deps.env instead.")]]
-void EnableMediaWithDefaultsAndTimeController(
-    TimeController& time_controller,
-    PeerConnectionFactoryDependencies& deps);
 
 }  // namespace webrtc
 
